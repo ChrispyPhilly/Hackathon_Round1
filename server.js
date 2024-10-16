8a17a720d8d4c1f286b53b9b3fa35665d37db78f
lh-25 <sprinklecake25@gmail.com>
2024-10-16 17:13:33 -0500

    first api call

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ app.use(bodyParser.json())
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
+
+app.get('/', (req,res) => res.send('welcome to General Assembly Mart!'))
+
 app.get('/items', itemController.getAllItems)
 app.get('/items/:id', itemController.getItemById)
-app.get('/items/', itemController.searchItemByName)
 app.get('/items',itemController.createItem)
 app.put('/items/:id', itemController.updateItem)
 app.delete('/items/:id', itemController.deleteItem)
